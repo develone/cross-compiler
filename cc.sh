51652753c7a60e158bec0fc2a698acad819c04c4
Edward Vidal Jr <develone@sbcglobal.net>
2019-06-04 16:20:54 -0600

    initial version

---
+++ cc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH==/opt/cross-pi-compile/bin/:$PATH
+export CC=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-gcc
+export AR=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-ar       
+export AS=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-as
