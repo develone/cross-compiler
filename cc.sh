2fc623d0178555bf563a76f32bf3d8d3611ae447
Edward Vidal Jr <develone@sbcglobal.net>
2019-06-04 16:51:16 -0600

    fix of PATH

--- cc.sh
+++ cc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PATH==/opt/cross-pi-compile/bin/:$PATH
+export PATH=/opt/cross-pi-compile/bin/:$PATH
 export CC=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-gcc
 export AR=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-ar       
 export AS=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-as
+
+
