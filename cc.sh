30edd961e8dc31e76841b60fc74e0e3ffba9f3c4
Edward Vidal Jr <develone@sbcglobal.net>
2019-06-05 06:24:34 -0600

    testing cross compiling of example programs and running on RPi3B

--- cc.sh
+++ cc.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-export PATH=/opt/cross-pi-compile/bin/:$PATH
-export CC=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-gcc
-export AR=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-ar       
-export AS=/opt/cross-pi-compile/bin/arm-linux-gnueabihf-as
+echo $PATH
+PATH=/opt/cross-pi-gcc/bin/:$PATH
+echo $PATH
+which arm-linux-gnueabihf-gcc
+echo
+cd test-cross-compiler
+rm -f *.o gfortranhw
+arm-linux-gnueabihf-gcc -c lifting.c
+arm-linux-gnueabihf-gcc lifting.o pi_jpeg.c -o pi_jpeg
+file pi_jpeg
+#note when the c++17 examples programs are cross compiled  
+#The following 2 programs need on the RPi3B 
+#< export PATH=/opt/gcc-9.1.0/bin:$PATH
+#< export LD_LIBRARY_PATH=/opt/gcc-9.1.0/lib:$LD_LIBRARY_PATH
+#links in /usr/include & /usr/lib
+#and the gcc-9.1.0-armhf-raspbian.tar.tjz extracted in /opt  
+arm-linux-gnueabihf-g++ -std=c++17 -Wall -pedantic if_test.cpp -o if_test
+file if_test 
 
+arm-linux-gnueabihf-g++ -std=c++17 -Wall -pedantic fs_test.cpp -o fs_test
+file fs_test 
 
+arm-linux-gnueabihf-gfortran helloworld.f -o gfortranhw
+file gfortranhw
